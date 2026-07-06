7d47a67611fcfa9fd3edd75d1dac60ab357ee387
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-07-06 23:35:07 +0600

    Add multiple catch example for exception handling
    Implemented multiple catch blocks to handle different exception types.

---
+++ Exeption Handling/Multiple Catch Example.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try
+    {
+        int choice;
+        cout << "Enter a number: ";
+        cin >> choice;
+
+        if (choice == 1)
+            throw 100;
+
+        else if (choice == 2)
+            throw 'A';
+
+        else
+            cout << "No Exception." << endl;
+    }
+
+    catch (int x)
+    {
+        cout << "Integer Exception: " << x << endl;
+    }
+
+    catch (char c)
+    {
+        cout << "Character Exception: " << c << endl;
+    }
+
+    return 0;
+}
